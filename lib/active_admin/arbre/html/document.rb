a3b256f3c95382e93638fc9869990043be59789f
Brett McHargue <brett.mchargue@headshift.com>
2011-09-15 13:29:00 +0100

    Change DOCTYPE

--- lib/active_admin/arbre/html/document.rb
+++ lib/active_admin/arbre/html/document.rb
@@ -18,7 +18,7 @@ module Arbre
       end
 
       def doctype
-        '<!DOCTYPE html>'.html_safe
+        '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'.html_safe
       end
 
       def to_html
